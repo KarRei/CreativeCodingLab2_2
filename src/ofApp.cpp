d0ea9c710ff5431f61acc102d10e96dbb0ff292f
karin <karin_lr@hotmail.com>
2017-02-08 18:24:53 +0000

    cube in center

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,15 +3,22 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
-    mesh.addVertex ( ofVec3f ( 0 , 0 ));
-    mesh.addColor ( ofColor::blue );
-    mesh.addVertex ( ofVec3f ( ofGetWidth() , 0 ));
-    mesh.addColor ( ofColor::yellow );
-    mesh.addVertex ( ofVec3f ( ofGetWidth() , ofGetHeight()));
-    mesh.addColor ( ofColor::green );
-    mesh.addVertex ( ofVec3f ( 0 , ofGetHeight()));
-    mesh.addColor ( ofColor::red );
-    mesh.setMode (OF_PRIMITIVE_TRIANGLE_FAN );
+    mesh.addVertex( ofVec3f ( 0 , 0 ));
+    mesh.addColor( ofColor::blue );
+    mesh.addVertex( ofVec3f ( ofGetWidth() , 0 ));
+    mesh.addColor( ofColor::yellow );
+    mesh.addVertex( ofVec3f ( ofGetWidth() , ofGetHeight()));
+    mesh.addColor( ofColor::green );
+    mesh.addVertex( ofVec3f ( 0 , ofGetHeight()));
+    mesh.addColor( ofColor::red );
+    mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN );
+    
+    mesh2.addVertex(ofVec3f((ofGetWidth()/2)-40 , (ofGetHeight()/2)-40 ));
+    mesh2.addVertex(ofVec3f((ofGetWidth()/2)-40 , (ofGetHeight()/2)+40 ));
+    mesh2.addVertex(ofVec3f((ofGetWidth()/2)+40 , (ofGetHeight()/2)+40 ));
+    mesh2.addVertex(ofVec3f((ofGetWidth()/2)+40 , (ofGetHeight()/2)-40 ));
+    //mesh2.setMode(OF_PRIMITIVE_LINE_LOOP); // Line cube
+    mesh2.setMode(OF_PRIMITIVE_TRIANGLE_FAN); // Filled cube
 }
 
 //--------------------------------------------------------------
@@ -22,6 +29,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     mesh.draw();
+    mesh2.draw();
 }
 
 //--------------------------------------------------------------
